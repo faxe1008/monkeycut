a68faa40a3834e253874a1c88d9fe0682864aafe
faxe1008 <fabianblatz@gmail.com>
2026-08-16 19:56:23 +0200

    core: avoid __int128 on MSVC in Time math

--- src/core/Time.cpp
+++ src/core/Time.cpp
@@ -41,6 +41,7 @@ Time Time::toTimebase(int tbNum, int tbDen) const
 {
     if (!isValid() || tbNum <= 0 || tbDen <= 0)
         return {};
+#if defined(__SIZEOF_INT128__)
     const __int128 n = static_cast<__int128>(m_ticks) * static_cast<__int128>(m_tbNum)
         * static_cast<__int128>(tbDen);
     const __int128 d = static_cast<__int128>(m_tbDen) * static_cast<__int128>(tbNum);
@@ -53,6 +54,21 @@ Time Time::toTimebase(int tbNum, int tbDen) const
     if (neg)
         q = -q;
     return Time(q, tbNum, tbDen);
+#else
+    const long double n = static_cast<long double>(m_ticks) * static_cast<long double>(m_tbNum)
+        * static_cast<long double>(tbDen);
+    const long double d = static_cast<long double>(m_tbDen) * static_cast<long double>(tbNum);
+    if (d == 0.0L)
+        return {};
+    const long double scaled = n / d;
+    const long double maxV = static_cast<long double>(std::numeric_limits<qint64>::max());
+    const long double minV = static_cast<long double>(std::numeric_limits<qint64>::min());
+    if (scaled >= maxV)
+        return Time(std::numeric_limits<qint64>::max(), tbNum, tbDen);
+    if (scaled <= minV)
+        return Time(std::numeric_limits<qint64>::min(), tbNum, tbDen);
+    return Time(static_cast<qint64>(scaled), tbNum, tbDen);
+#endif
 }
 
 Time Time::operator+(const Time& o) const
@@ -70,8 +86,15 @@ Time Time::operator-(const Time& o) const
 bool Time::operator<(const Time& o) const
 {
     if (isValid() && o.isValid()) {
+#if defined(__SIZEOF_INT128__)
         const __int128 a = static_cast<__int128>(m_ticks) * o.m_tbNum * o.m_tbDen;
         const __int128 b = static_cast<__int128>(o.m_ticks) * m_tbNum * m_tbDen;
+#else
+        const long double a = static_cast<long double>(m_ticks) * static_cast<long double>(o.m_tbNum)
+            * static_cast<long double>(o.m_tbDen);
+        const long double b = static_cast<long double>(o.m_ticks) * static_cast<long double>(m_tbNum)
+            * static_cast<long double>(m_tbDen);
+#endif
         return a < b;
     }
     return o.isValid();
@@ -80,8 +103,15 @@ bool Time::operator<(const Time& o) const
 bool Time::operator<=(const Time& o) const
 {
     if (isValid() && o.isValid()) {
+#if defined(__SIZEOF_INT128__)
         const __int128 a = static_cast<__int128>(m_ticks) * o.m_tbNum * o.m_tbDen;
         const __int128 b = static_cast<__int128>(o.m_ticks) * m_tbNum * m_tbDen;
+#else
+        const long double a = static_cast<long double>(m_ticks) * static_cast<long double>(o.m_tbNum)
+            * static_cast<long double>(o.m_tbDen);
+        const long double b = static_cast<long double>(o.m_ticks) * static_cast<long double>(m_tbNum)
+            * static_cast<long double>(m_tbDen);
+#endif
         return a <= b;
     }
     return !o.isValid();
@@ -90,8 +120,15 @@ bool Time::operator<=(const Time& o) const
 bool Time::operator==(const Time& o) const
 {
     if (isValid() && o.isValid()) {
+#if defined(__SIZEOF_INT128__)
         const __int128 a = static_cast<__int128>(m_ticks) * o.m_tbNum * o.m_tbDen;
         const __int128 b = static_cast<__int128>(o.m_ticks) * m_tbNum * m_tbDen;
+#else
+        const long double a = static_cast<long double>(m_ticks) * static_cast<long double>(o.m_tbNum)
+            * static_cast<long double>(o.m_tbDen);
+        const long double b = static_cast<long double>(o.m_ticks) * static_cast<long double>(m_tbNum)
+            * static_cast<long double>(m_tbDen);
+#endif
         return a == b;
     }
     return !isValid() && !o.isValid();
