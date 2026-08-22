830a4bb4b2b3fc4ef7437326b5b6fe2e76414c04
Fabian Blatz <fabianblatz@gmail.com>
2026-08-22 10:44:34 +0200

    Move load more to button

--- src/ui/CutlistAtDialog.cpp
+++ src/ui/CutlistAtDialog.cpp
@@ -65,10 +65,15 @@ CutlistAtDialog::CutlistAtDialog(QWidget* parent)
     connect(buttons, &QDialogButtonBox::rejected, this, &QDialog::reject);
     connect(buttons, &QDialogButtonBox::accepted, this, &QDialog::accept);
 
+    m_moreBtn = new QPushButton(tr("Load more"), this);
+    m_moreBtn->setEnabled(false);
+    connect(m_moreBtn, &QPushButton::clicked, this, &CutlistAtDialog::nextPage);
+
     auto* layout = new QVBoxLayout(this);
     layout->addLayout(row);
     layout->addWidget(m_table, 1);
     layout->addWidget(m_status);
+    layout->addWidget(m_moreBtn);
     layout->addWidget(buttons);
     resize(860, 420);
 }
@@ -101,6 +106,7 @@ void CutlistAtDialog::doSearch()
     m_queryStr = query;
     m_page = 0;
     m_hasMore = false;
+    m_moreBtn->setEnabled(false);
     m_table->clear();
     setStatus(tr("Searching “%1”…").arg(query));
     m_client->search(query, 0);
@@ -110,6 +116,7 @@ void CutlistAtDialog::nextPage()
 {
     if (m_page < 0 || !m_hasMore)
         return;
+    m_moreBtn->setEnabled(false);
     ++m_page;
     m_client->search(m_queryStr, m_page);
 }
@@ -119,6 +126,7 @@ void CutlistAtDialog::onResults(const SearchPage& page)
     appendResults(page);
     m_hasMore = page.hasMore;
     m_page = page.page;
+    m_moreBtn->setEnabled(m_hasMore);
     const int n = m_table->topLevelItemCount();
     if (n == 0)
         setStatus(tr("No cutlists found for “%1”.").arg(m_queryStr));
@@ -145,14 +153,6 @@ void CutlistAtDialog::appendResults(const SearchPage& page)
         item->setData(0, Qt::UserRole + 2, it.otrkey);
         item->setData(0, Qt::UserRole + 3, it.airDate);
     }
-    // "Next page" entry at the bottom
-    auto* more = new QTreeWidgetItem(m_table);
-    more->setText(0, page.hasMore ? tr("… load more …")
-                                  : tr("… no more results …"));
-    more->setData(0, Qt::UserRole, page.hasMore ? -1 : 0);
-    if (!page.hasMore)
-        more->setFlags(more->flags() & ~Qt::ItemIsSelectable
-                       & ~Qt::ItemIsEnabled);
     m_table->setUpdatesEnabled(true);
 }
 
@@ -160,10 +160,6 @@ void CutlistAtDialog::onItemActivated(QTreeWidgetItem* item, int column)
 {
     Q_UNUSED(column)
     const qint64 id = item->data(0, Qt::UserRole).toLongLong();
-    if (id == -1) {
-        nextPage(); // "load more" row
-        return;
-    }
     if (id <= 0)
         return;
     m_activeItem = item;
